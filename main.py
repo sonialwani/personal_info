dba5717ac5f24b104b9d50bb3998aca72febc557
sonialwani <alwani.soni@gmail.com>
2023-09-02 14:30:12 +0530

    added photos of all apps

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import streamlit as st
-
+import pandas as pd
 st.set_page_config(layout="wide")
 
 col1, col2 = st.columns(2)
@@ -17,3 +17,24 @@ with col2:
 content2="Please feel free to reach out to me in case of any help or guidance"
 
 st.write(content2)
+
+df = pd.read_csv("data (1).csv",sep=";")
+
+col3, col4 = st.columns(2)
+
+
+
+
+with col3:
+  for i in range (0,11):
+        st.header(df['title'][i])
+        st.write(df['description'][i])
+        st.image(df['image'][i])
+        st.write("[APP](https://sonialwani.github.io/todo-app/)")
+
+with col4:
+  for i in range (11,20):
+        st.header(df['title'][i])
+        st.write(df['description'][i])
+        st.image(df['image'][i])
+        st.write("[APP](https://sonialwani.github.io/todo-app/)")
